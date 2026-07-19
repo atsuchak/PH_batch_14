6ee1b160506ab43063d380c7d9b8c0a474626182
atsuchak <ahnaftajwarsuchak@gmail.com>
2026-07-19 22:55:14 +0600

    Add practice tasks and examples for conditionals and decision making in Module 8

---
+++ Module 8/compare.js
@@ -0,0 +1,14 @@
+/* 
+    1. Greater than: >
+    2. Less than: <
+    3. Equal: == ===
+
+    4. Greater or equal: >=
+    5. Less or equal: <=
+    6. Not equal: !=, !==
+
+    7. And: &&
+    8. Or: ||
+*/
+
+console.log(6 > 3);
