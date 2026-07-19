37e488091ff6e9d87d332499d58ae8f421dd1a10
atsuchak <ahnaftajwarsuchak@gmail.com>
2026-07-19 22:54:53 +0600

    Add convert_type.js and math.js for type conversion and basic math operations

---
+++ Module 7/math.js
@@ -0,0 +1,19 @@
+console.log(7);
+
+
+// Variable
+let num = 0;
+let name = "Suchak";
+const PI = 3.1415;
+
+console.log(num);
+console.log(PI);
+
+num = 7;
+console.log(num);
+
+// type of a variable
+confirm.log(typeof name);
+
+let isHuman = true;
+console.log(isHuman);
