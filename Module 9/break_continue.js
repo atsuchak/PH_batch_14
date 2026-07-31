5fe0a3253466a23b3301c602b270ff87241f10ab
atsuchak <ahnaftajwarsuchak@gmail.com>
2026-07-31 20:03:55 +0600

    added module 9

---
+++ Module 9/break_continue.js
@@ -0,0 +1,15 @@
+//break
+for(let i = 0; i < 100; i++) {
+    if(i === 25) break;
+
+    console.log(i, ". Hello world");
+}
+
+console.log();
+
+//continue
+for(let i = 0; i < 10; i++) {
+    if(i === 5) continue;
+
+    console.log(i);
+}
