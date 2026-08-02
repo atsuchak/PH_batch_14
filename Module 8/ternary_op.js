c08c7e7d2d4cba30a45decfa63411db86943f6ab
atsuchak <ahnaftajwarsuchak@gmail.com>
2026-08-03 00:11:38 +0600

    Clean up old duplicate folder names

--- Module 8/ternary_op.js
+++
@@ -1,7 +0,0 @@
-/* 
-    condition? if true : if false   
-*/
-
-let age = 19;
-
-age >= 18? console.log("Able to vote") : console.log("Can't vote");
--- Module 9/do_while.js
+++
@@ -1,5 +0,0 @@
-let i = 0;
-do{
-    console.log("Hello world");
-    i++;
-} while(i < 10)
